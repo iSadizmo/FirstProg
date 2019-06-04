17aec4387732f54e97e4530c16aae9f9a4610384
Artem <isadizmo@gmail.com>
2019-06-04 15:54:32 +0300

    Это наш первый коммит

---
+++ Java1.js
@@ -0,0 +1,48 @@
+`use strict`;
+
+a = 5;
+console.log(a);
+//alert("привет");
+// "comment"
+/*"comment"*/
+console.log(4/0);
+console.log(`streng`*9);
+let something;
+console.log(something)
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+}
+console.log(persone["name"]);
+let arr = [ `plam.png`,`orange.jpg`, `apple.bmp`];
+
+console.log (arr[0])
+
+//let answer = confirm("Are you here?")
+
+//console.log (answer);
+//let answer = +prompt("Есть ли Вам 18 лет?" , "да");
+
+//console.log (typeof(answer));
+//console.log ("arr"+ "- object")
+//console.log (4 + +"- object")
+ let incr = 10, 
+     decr = 10 ;
+
+console.log (incr++);
+console.log (decr--);
+
+console.log (5%2);
+
+console.log ("2" === 2);
+
+let ischecked = true,
+    isclose = false;
+
+    console.log (ischecked || !
+        
+        
+        isclose);
+
+    console.log ();
